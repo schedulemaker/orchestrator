7983b9581ec3c85bd3fb61e6634a47d999da6c2b
Paul Hutchings <paul@xps>
2020-04-22 09:29:33 -0400

    Added support for paths and trip planner

--- index/index.js
+++ index/index.js
@@ -8,7 +8,13 @@ if (typeof(cache) === 'undefined'){
 module.exports.handler = async function(event, context){
     const schedules = await runScheduler(event);
     const [distances, commutes] = await Promise.all(runPaths(schedules), runTripPlanner(schedules));
-
+    return schedules.map((s, idx) => {
+        return {
+            sections: s,
+            commute: commutes[idx],
+            totalDistance: distances[idx]['totalDistance']
+        }
+    });
 }
 
 async function runScheduler(event){
@@ -23,14 +29,22 @@ async function runScheduler(event){
 }
 
 async function runPaths(schedules){
-    const params = {
-        FunctionName: process.env.PATHS,
-
-    }
+    return await Promise.all(schedules.map(async s => {
+        const params = {
+            FunctionName: process.env.PATHS,
+            Payload: JSON.stringify(s)
+        }
+        const response = await cache.Lambda.invoke(params).promise();
+        return JSON.parse(response.Payload);
+    }));
 }
 
 async function runTripPlanner(schedules){
     const params = {
-        FunctionName: process.env.TRIP
+        FunctionName: process.env.TRIP,
+        Payload: JSON.stringify(schedules)
     }
+    const response = await cache.Lambda.invoke(params).promise();
+    const data = JSON.parse(response.Payload);
+    return data.map(s => s.pop()['tripPlanner']);
 }
